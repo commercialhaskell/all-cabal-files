ebd7fab0b0027b308680b34e813227c639e82f90
Michael Snoyman <michael@snoyman.com>
2015-04-13 12:49:21 +0300

    Forgot to update the update.sh script

--- update.sh
+++ update.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-set -e
 set -x
+set -e
 
 rm -f 00-index.tar.gz
 wget http://hackage.haskell.org/packages/archive/00-index.tar.gz
 tar xf 00-index.tar.gz
 rm -f 00-index.tar.gz
-git add -A
-git commit -m "Update from hackage at $(date)"
-git push git@github.com:commercialhaskell/all-cabal-files.git HEAD:master
+
+if [ -n "$(git status --porcelain)" ]
+then
+    git add -A
+    git commit -m "Update from hackage at $(date)"
+    git push git@github.com:commercialhaskell/all-cabal-files.git HEAD:master
+else
+    echo No changes present
+fi
