133cd026f8a1f99d719d97fcf884372ded173655
Michael Snoyman <michael@snoyman.com>
2015-04-16 14:11:24 +0300

    Add make-index.sh

---
+++ make-index.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+DIR=$HOME/.cabal/packages/hackage.haskell.org
+TAR=$DIR/00-index.tar
+TARGZ=$TAR.gz
+
+git pull
+mkdir -p "$DIR"
+
+rm -f $TAR $TARGZ
+
+git archive --format=tar -o "$TAR" master
+gzip -k "$TAR"
