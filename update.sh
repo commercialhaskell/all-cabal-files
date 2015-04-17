ae4b054e374d21e40d8878d2d8e24b3418b0ab51
Michael Snoyman <michael@snoyman.com>
2015-04-17 05:21:15 +0300

    Push to hackage branch as well #2

--- update.sh
+++ update.sh
@@ -13,6 +13,7 @@ then
     git add -A
     git commit -m "Update from Hackage at $(date)" --gpg-sign=D6CF60FD
     git push git@github.com:commercialhaskell/all-cabal-files.git HEAD:master
+    git push git@github.com:commercialhaskell/all-cabal-files.git HEAD:hackage
 else
     echo No changes present
 fi
